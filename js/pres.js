aba68939e79fe1d0e19095abf18665b069c77c63
Per Liedman <per@liedman.net>
2016-03-30 23:45:34 +0200

    Almost there

--- js/pres.js
+++ js/pres.js
@@ -19,9 +19,10 @@ window.standardMap = (function() {
 
     return {
         start: function() {
-            map = L.map('standard-map').setView([40.420537,-3.7066242], 18);
-            L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + token, {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            map = L.map('standard-map').setView([40.420537,-3.7066242], 19);
+            L.tileLayer('https://api.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=' + token, {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                maxZoom: 20
             }).addTo(map);
         },
         stop: function() {
@@ -82,6 +83,175 @@ window.utfGrid = (function() {
                     .setView([0, 0], 1)
                     .addLayer(tiles)
                     .addLayer(utfGrid);
+        },
+        stop: function() {
+            map.remove();
+        }
+    };
+})();
+
+window.simpleUnderneath = (function() {
+    var map;
+
+    return {
+        start: function() {
+            map = L.map('simple-underneath').setView([40.420537,-3.7066242], 18);
+            var featureLayer = L.geoJson(undefined, {
+                    pointToLayer: function(f, latLng) {
+                        return L.circleMarker(latLng, {
+                                radius: 4,
+                                opacity: 1,
+                                fillOpacity: 0.5
+                            })
+                            .bindPopup('<li><span class="maki-icon ' + f.properties.maki + '"></span>' + f.properties.name + '</li>');
+                    }
+                }).addTo(map);
+
+            L.tileLayer('https://api.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=' + token, {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            var pois = L.tileLayer.underneath('http://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/' +
+                        '{z}/{x}/{y}.vector.pbf?access_token=' + token, {
+                    layers: ['poi_label'],
+                    lazy: true,
+                    zoomIn: 0
+                })
+                .addTo(map);
+
+            map.on('click', function(e) {
+                var content = '<h2>Nearby</h2> <ul>',
+                    showResults = function(results) {
+                        featureLayer.addData(results);
+                        for (var i = 0; i < results.length; i++) {
+                            var f = results[i];
+                            content += '<li><span class="maki-icon ' +
+                                f.properties.maki + '"></span>' +
+                                f.properties.name +
+                                '</li>';
+                        }
+
+                        content += '</ul>';
+
+                        L.popup()
+                            .setLatLng(e.latlng)
+                            .setContent(content)
+                            .openOn(map);
+                    };
+
+                featureLayer.clearLayers();
+                pois.query(e.latlng, function(err, results) {
+                    if (results.length > 0) {
+                        results = results.splice(0, 5);
+                        showResults(results);
+                    }
+                }, null, 50);
+            });
+        },
+        stop: function() {
+            map.remove();
+        }
+    };
+})();
+
+window.advancedUnderneath = (function() {
+    var map;
+
+    var getOsmObject = function(type, osmId, cb) {
+        fetch('http://www.openstreetmap.org/api/0.6/' + type + '/' + osmId, {
+            mode: 'cors'
+        }).then(function(response) {
+            response.text().then(function(text) {
+                var data = new window.DOMParser().parseFromString(text, 'text/xml'),
+                    tagTags = data.getElementsByTagName('tag'),
+                    tags = {},
+                    i,
+                    t;
+
+                for (i = 0; i < tagTags.length; i++) {
+                    t = tagTags[i];
+                    tags[t.getAttribute('k')] = t.getAttribute('v');
+                }
+
+                cb(null, tags);
+            });
+        }, function(err) {
+            cb(err);
+        });
+    };
+
+
+    return {
+        start: function() {
+            map = L.map('advanced-underneath').setView([40.420537,-3.7066242], 18);
+            var featureLayer = L.geoJson(undefined, {
+                    pointToLayer: function(f, latLng) {
+                        return L.circleMarker(latLng, {
+                                radius: 4,
+                                opacity: 1,
+                                fillOpacity: 0.5
+                            })
+                            .bindPopup('<li><span class="maki-icon ' + f.properties.maki + '"></span>' + f.properties.name + '</li>');
+                    }
+                }).addTo(map);
+
+            L.tileLayer('https://api.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=' + token, {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            var pois = L.tileLayer.underneath('http://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/' +
+                        '{z}/{x}/{y}.vector.pbf?access_token=' + token, {
+                    layers: ['poi_label'],
+                    lazy: true,
+                    zoomIn: 0
+                })
+                .addTo(map);
+
+            map.on('click', function(e) {
+                featureLayer.clearLayers();
+                pois.query(e.latlng, function(err, results) {
+                    if (results.length > 0) {
+                        results = results.splice(0, 5);
+
+                        featureLayer.addData(results);
+                        var content = L.DomUtil.create('div', ''),
+                            header = L.DomUtil.create('h2', '', content),
+                            list = L.DomUtil.create('ul', '', content);
+
+                        header.innerText = 'Nearby';
+
+                        results.forEach(function(f) {
+                            var item = L.DomUtil.create('li', '', list),
+                                icon = L.DomUtil.create('span', 'maki-icon ' + f.properties.maki, item),
+                                a = L.DomUtil.create('a', '', item);
+
+                            a.innerText = f.properties.name;
+
+                            L.DomEvent.on(a, 'click', function(e) {
+                                getOsmObject('node', f.id - 1000000000000000, function(err, osmFeature) {
+                                    if (err) {
+                                        return console.warn(err);
+                                    }
+
+                                    var c = f.geometry.coordinates;
+
+                                    L.popup()
+                                        .setLatLng([c[1], c[0]])
+                                        .setContent('<pre>' + JSON.stringify(osmFeature, null, 2) + '</pre>')
+                                        .openOn(map);
+                                });
+
+                                L.DomEvent.preventDefault(e);
+                            });
+                        });
+
+                        L.popup()
+                            .setLatLng(e.latlng)
+                            .setContent(content)
+                            .openOn(map);
+                    }
+                }, null, 50);
+            });
         },
         stop: function() {
             map.remove();
